af8d42cd8b0d69b70e3ce7163292d30097342224
Alexey Solodkiy <alexey@solodkiy.by>
2022-03-02 22:05:30 +0300

    --save

--- examples/client.php
+++ examples/client.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Solodkiy\AlfaBankRuClient\AlfaBankClient;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/_functions.php';
@@ -10,21 +9,51 @@ $config = require_once __DIR__ . '/_config.php';
 
 
 $getSms = function () {
-    $line = readline('Sms: ');
-    return $line ? (string)$line : null;
 };
 
 
+$x = new class implements \Solodkiy\AlfaBankRuClient\InteractionTrapInterface {
 
+    public function waitForSms(): ?string
+    {
+        $line = readline('Sms: ');
+        return $line ? (string)$line : null;
+    }
+};
+
+
+/*
 $logger = new SimpleLogger();
 $driver = createWebDriver($config['selenium_host'], $config['selenium_port']);
-$client = new AlfaBankClient($driver, $config['bank_login'], $config['bank_pass'], $getSms);
+$client = new AlfaBankWebClient($driver, $config['bank_login'], $config['bank_pass'], $x);
 $client->setLogger($logger);
 
 $accounts = $client->getAccountsList();
+var_dump($accounts);
+exit;
+*/
+
+
+//$cookie = readline('Cookie: ');
+//$token = readline('csrf-token: ');
+
+$cookie = '';
+$token = '';
+
+$headers = [
+    "Cookie: " . $cookie,
+    'x-csrf-token: ' . $token,
+];
+$client2 = new \Solodkiy\AlfaBankRuClient\AlfaBankApiClient($headers);
+//$r = $client2->downloadAccountHistory($accounts[0]->getNumber());
+$r = $client2->downloadAccountHistory('40820810604170002567', '2022-01-22', '2022-02-22');
+
+var_dump($r);
+exit;
+
 foreach ($accounts as $account) {
     $logger->info($account->getName() . ' (' . $account->getType() . '): ' . $account->getNumber());
-    $csv = $client->downloadAccountHistory($account->getNumber());
-    $logger->debug($csv);
+    $json = $client->downloadAccountHistory($account->getNumber());
+    $logger->debug($json);
 }
 
